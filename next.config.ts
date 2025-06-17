0e79cbbf6fde395246cd381040ecae2fe315d77f
Seokseong Jeon <6747391+somidad@users.noreply.github.com>
2025-06-18 00:33:52 +0900

    Enforce HSTS

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,31 @@
 import type { NextConfig } from "next";
+import { PHASE_PRODUCTION_BUILD } from "next/dist/shared/lib/constants";
+import { env } from "node:process";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  output: 'export',
-  distDir: 'docs'
+const nextConfig = (phase: string): NextConfig => {
+	const isProd =
+		phase === PHASE_PRODUCTION_BUILD || env.NODE_ENV === "production";
+
+	return {
+		/* config options here */
+		async headers() {
+			return [
+				{
+					source: "/(.*)",
+					headers: [
+						{
+							key: "Strict-Transport-Security",
+							value: isProd
+								? "max-age=31536000; includeSubDomains; preload"
+								: "max-age=0",
+						},
+					],
+				},
+			];
+		},
+		output: "export",
+		distDir: "docs",
+	};
 };
 
 export default nextConfig;
