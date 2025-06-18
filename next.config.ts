975f864398138dab633d75cb1e57115e32823f17
Seokseong Jeon <6747391+somidad@users.noreply.github.com>
2025-06-18 01:39:27 +0000

    Remove header settings from Next config

--- next.config.ts
+++ next.config.ts
@@ -1,50 +1,9 @@
 import type { NextConfig } from "next";
-import { PHASE_PRODUCTION_BUILD } from "next/dist/shared/lib/constants";
-import { env } from "node:process";
 
-const nextConfig = (phase: string): NextConfig => {
-	const isProd =
-		phase === PHASE_PRODUCTION_BUILD || env.NODE_ENV === "production";
-
-	return {
-		/* config options here */
-		async headers() {
-      const cspHeader = `
-        default-src 'self';
-        script-src 'self' https://apis.google.com https://accounts.google.com 'unsafe-inline';
-        frame-src 'self' https://content.googleapis.com;
-        connect-src 'self' https://gmail.googleapis.com;
-        style-src 'self' 'unsafe-inline';
-      `
-			return [
-				{
-					source: "/(.*)",
-					headers: [
-						{
-							key: "Strict-Transport-Security",
-							value: isProd
-								? "max-age=31536000; includeSubDomains; preload"
-								: "max-age=0",
-						},
-            {
-              key: 'Access-Control-Allow-Origin',
-              value: isProd ? 'https://gfilter.app' : '*',
-            },
-            {
-              key: "Content-Security-Policy",
-              value: cspHeader.replace(/\s{2,}/g, " ").trim(),
-            },
-            {
-              key: 'Cross-Origin-Opener-Policy',
-              value: 'same-origin',
-            }
-					],
-				},
-			];
-		},
-		output: "export",
-		distDir: "docs",
-	};
+const nextConfig: NextConfig = {
+	/* config options here */
+	output: "export",
+	distDir: "docs",
 };
 
 export default nextConfig;
