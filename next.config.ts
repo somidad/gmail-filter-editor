bd96f618b08228c497409131a7aa6b00cd875939
Seokseong Jeon <sjeon87@gmail.com>
2025-08-04 00:02:27 +0900

    Use .next/ for dev

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
+import { env } from "process";
 
 const nextConfig: NextConfig = {
-	/* config options here */
-	output: "export",
-	distDir: "docs",
+  /* config options here */
+  output: "export",
+  distDir: env.NODE_ENV === "production" ? "docs" : ".next",
 };
 
 export default nextConfig;
