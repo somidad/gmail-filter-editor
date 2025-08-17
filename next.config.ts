e24a07be22acee779b450e58560f27dfe7de114c
Seokseong Jeon <sjeon87@gmail.com>
2025-08-17 23:12:30 +0900

    Adjust Next.js distDir

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,14 @@ import { env } from "process";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  distDir: env.NODE_ENV === "production" ? "docs" : ".next",
+  distDir: env.GITHUB_ACTIONS ? "docs" : ".next",
+  // Truth table
+  // production | GH workflow | distDir
+  // -----------|-------------|----------------
+  // false      | false       | .next
+  // true       | false       | .next
+  // false      | true        | d/c (docs)
+  // true       | true        | docs
 };
 
 export default nextConfig;
